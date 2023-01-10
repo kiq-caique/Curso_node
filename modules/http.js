983a5fdb43aa43c9eebbd023ba06349c34c92a5a
kiq-caique <caique-kiq@hotmail.com>
2023-01-10 15:44:30 -0300

    '/users' adicionada

--- modules/http.js
+++ modules/http.js
@@ -18,6 +18,9 @@ const server = http.createServer((req, res) => {
         email: "camila@gmail.com",
       },
     ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
   }
 });
 
