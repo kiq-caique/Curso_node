b5332c04688803f0611f63b430692aae455f1501
kiq-caique <caique-kiq@hotmail.com>
2023-01-10 16:13:08 -0300

    '/users' com Express

--- modules/express.js
+++ modules/express.js
@@ -7,6 +7,21 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1>Hello world</h1>");
 });
 
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "Caique",
+      email: "caique@gmail.com",
+    },
+    {
+      name: "Danilo",
+      email: "danilo@gmail.com",
+    },
+  ];
+
+  res.status(200).json(users);
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}!`));
