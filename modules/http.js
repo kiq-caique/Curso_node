e75bdbff8486e53a9c1a050073c356d42633868e
kiq-caique <caique-kiq@hotmail.com>
2023-01-10 16:09:08 -0300

    criando servidor com Express

--- modules/http.js
+++ modules/http.js
@@ -5,7 +5,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   if (req.url === "/home") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>home page<h1>");
+    res.end("<h1>home page</h1>");
   }
   if (req.url === "/users") {
     const users = [
